7db8d383f34f1d60d8d71c2b00d77aa3fac638c1
billybrown237 <kano.billybrown@gmail.com>
2024-05-19 18:37:32 +0100

    chore:mod next.config.js to accomodate broken images

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -9,7 +9,7 @@ import SearchManufacturer from "./SearchManufacturer";
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
-      src={"/magnifying-glass.svg"}
+      src={"/car_showcase/magnifying-glass.svg"}
       alt={"magnifying glass"}
       width={40}
       height={40}

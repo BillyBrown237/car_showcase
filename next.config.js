9f93e9e92af5edd806324f0afc38e69eba003c13
billybrown237 <kano.billybrown@gmail.com>
2024-05-19 18:33:59 +0100

    chore:mod next.config.js to accomodate broken images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ["cdn.imagin.studio"],
   },
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/carcar_showcase",
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
